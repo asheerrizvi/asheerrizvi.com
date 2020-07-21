8c00945d4af283cf36153785a6d3dad7dbffe1cd
asheerrizvi <asheer.vil@gmail.com>
2020-07-21 14:04:49 +0530

    Minor changes to font-sizes and styling in blog-posts.js

--- src/components/blog-posts.js
+++ src/components/blog-posts.js
@@ -10,10 +10,10 @@ const BlogPosts = ({ posts }) => {
           <Link to={node.fields.slug}>
             <article key={node.fields.slug} className="group mt-6 lg:-ml-4 lg:px-4 lg:py-2 lg:hover:bg-indigo-100 rounded-md">
               <div>
-                <span className="block text-gray-800">
+                <span className="block text-sm lg:text-lg text-gray-800">
                   {node.frontmatter.date}
                 </span>
-                <span className="text-xl lg:text-2xl">{title}</span>
+                <span className="font-medium text-xl lg:text-2xl">{title}</span>
               </div>
             </article>
           </Link>
