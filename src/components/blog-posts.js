c89de391271851bdf8c44c42d9a01fe5740b7a48
asheerrizvi <asheer.vil@gmail.com>
2020-07-21 13:28:34 +0530

    Footer completed, grouped the date, and the blog title in blog-posts.js added a hover style for lg devices for visual representation.

--- src/components/blog-posts.js
+++ src/components/blog-posts.js
@@ -7,16 +7,16 @@ const BlogPosts = ({ posts }) => {
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
-          <article key={node.fields.slug} className="mt-6">
-            <div>
-              <span className="block text-gray-800">
-                {node.frontmatter.date}
-              </span>
-              <Link className="text-xl lg:text-2xl" to={node.fields.slug}>
-                {title}
-              </Link>
-            </div>
-          </article>
+          <Link to={node.fields.slug}>
+            <article key={node.fields.slug} className="group mt-6 lg:-ml-4 lg:px-4 lg:py-2 lg:hover:bg-indigo-100 rounded-md">
+              <div>
+                <span className="block text-gray-800">
+                  {node.frontmatter.date}
+                </span>
+                <span className="text-xl lg:text-2xl">{title}</span>
+              </div>
+            </article>
+          </Link>
         )
       })}
     </React.Fragment>
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,28 +1,71 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import gatsby from "../../content/assets/gatsby-logo.svg"
+import netlify from "../../content/assets/netlify-logo.svg"
+import github from "../../content/assets/github-logo.svg"
+import twitter from "../../content/assets/twitter-logo.svg"
+
 const Footer = () => {
   return (
     <footer>
-      <nav className="flex mt-16 justify-center lg:justify-start lg:-ml-4">
+      <nav className="flex mt-16 justify-center md:justify-start md:-ml-8">
         <Link
           to="/"
-          className="font-medium hover:bg-indigo-100 text-xl text-indigo-500 px-4 py-2 mx-auto lg:mx-0 lg:mr-12 focus:outline-none rounded-md"
+          className="lg:hover:bg-indigo-100 text-xl text-indigo-500 px-4 py-2 md:px-8 md:py-4 mx-auto md:mx-0 focus:outline-none rounded-md"
         >
           Home
         </Link>
         <Link
           to="/blog"
-          className="font-medium hover:bg-indigo-100 text-xl text-indigo-500 px-4 py-2 mx-auto lg:mx-0 lg:mr-12 focus:outline-none rounded-md"
+          className="lg:hover:bg-indigo-100 text-xl text-indigo-500 px-4 py-2 md:px-8 md:py-4 mx-auto md:mx-0 focus:outline-none rounded-md"
         >
           Blog
         </Link>
         <Link
           to="/about"
-          className="font-medium hover:bg-indigo-100 text-xl text-indigo-500 px-4 py-2 mx-auto lg:mx-0 lg:mr-12 focus:outline-none rounded-md"
+          className="lg:hover:bg-indigo-100 text-xl text-indigo-500 px-4 py-2 md:px-8 md:py-4 mx-auto md:mx-0 focus:outline-none rounded-md"
         >
           About
         </Link>
+      </nav>
+      <nav className="flex mt-8 justify-center md:justify-start md:-ml-6">
+        <a
+          href="https://www.gatsbyjs.org/"
+          title="Built with Gatsby"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block max-w-full h-auto mx-3 md:mx-6 mx-auto md:mx-0"
+        >
+          <img src={gatsby} className="h-8 w-8" alt="Gatsby" />
+        </a>
+        <a
+          href="https://www.netlify.com/"
+          title="Hosted on Netlify"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block max-w-full h-auto mx-3 md:mx-6 mx-auto md:mx-0"
+        >
+          <img src={netlify} className="h-8 w-8" alt="Netlify" />
+        </a>
+        <a
+          href="https://www.github.com/asheerrizvi"
+          title="Asheer Rizvi on Github"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block max-w-full h-auto mx-3 md:mx-6 mx-auto md:mx-0"
+        >
+          <img src={github} className="h-8 w-8" alt="Github" />
+        </a>
+        <a
+          href="https://www.twitter.com/rizvi_asheer"
+          title="Asheer Rizvi on Twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block max-w-full h-auto mx-3 md:mx-6 mx-auto md:mx-0"
+        >
+          <img src={twitter} className="h-8 w-8" alt="Twitter" />
+        </a>
       </nav>
     </footer>
   )
