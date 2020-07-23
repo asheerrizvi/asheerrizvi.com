a569abbd51a7d2bd3736fbf7a7f2b62b4efb96b0
asheerrizvi <asheer.vil@gmail.com>
2020-07-23 23:06:56 +0530

    Added inline style for blog-post instead of a separate CSS file, overwritten border and box-shadow styles for images and code snippets in tailwind.config.js.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,7 +3,6 @@ import { graphql, navigate } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import styles from "./blog-post.module.css"
 
 const BlogPostTemplate = ({ data, pageContext }) => {
   const post = data.markdownRemark
@@ -16,8 +15,8 @@ const BlogPostTemplate = ({ data, pageContext }) => {
   return (
     <Layout>
       <SEO title={post.frontmatter.title} />
-      <h2 className={styles.title}>{post.frontmatter.title}</h2>
-      <p>{post.frontmatter.date}</p>
+      <h2 style={{ marginBottom: "0.75rem" }}>{post.frontmatter.title}</h2>
+      <span>{post.frontmatter.date}</span>
       <article>
         <section dangerouslySetInnerHTML={{ __html: post.html }} />
         <hr />
@@ -26,7 +25,7 @@ const BlogPostTemplate = ({ data, pageContext }) => {
       <nav className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         {previous ? (
           <button
-            className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-semibold py-4 px-4 rounded-md inline-flex items-center justify-center"
+            className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-semibold py-4 px-4 rounded-lg inline-flex items-center justify-center"
             onClick={() => navigateToPost(previous.fields.slug)}
           >
             {previous.frontmatter.title}
@@ -36,7 +35,7 @@ const BlogPostTemplate = ({ data, pageContext }) => {
         )}
         {next ? (
           <button
-            className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-semibold py-4 px-4 rounded-md inline-flex items-center justify-center"
+            className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-semibold py-4 px-4 rounded-lg inline-flex items-center justify-center"
             onClick={() => navigateToPost(next.fields.slug)}
           >
             {next.frontmatter.title}
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,16 @@ module.exports = {
             color: "#805AD5",
             textDecoration: "none",
           },
+          "img[class*=gatsby-resp-image-image]": {
+            borderRadius: "0.375rem",
+            boxShadow:
+              "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05) !important",
+          },
+          "pre[class*=language-]": {
+            borderRadius: "0.375rem",
+            boxShadow:
+              "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+          },
         },
       },
     },
