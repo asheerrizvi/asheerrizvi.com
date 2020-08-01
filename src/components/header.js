d1f953c92564b0d230e40c9ad9672884cc0607ce
asheerrizvi <asheer.vil@gmail.com>
2020-08-01 20:32:43 +0530

    Changed the styling for the site title, added active styling for nav links.

--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,34 @@ import React from "react"
 import { Link } from "gatsby"
 
 const Header = ({ title }) => {
+  title = title
+    .split(" ")
+    .map((el, index) =>
+      index === 1 ? el.replace(/^\w/, c => c.toUpperCase()) : el.toLowerCase()
+    )
+    .join("")
   return (
     <header className="mb-16">
       <nav className="flex items-baseline justify-between flex-no-wrap">
         <div className="flex items-center flex-shrink-0">
-          <h3 className="text-3xl text-purple-600">
-            <Link to={`/`}>{title}</Link>
-          </h3>
+          <Link className="text-2xl md:text-4xl text-purple-600" to={`/`}>
+            {title}
+          </Link>
         </div>
         <div>
-          <Link className="mr-3 md:mr-6 text-2xl text-purple-600" to={`/blog`}>
-            blog
+          <Link
+            className="text-xl md:text-2xl text-purple-600 pb-1 mr-4 md:mr-6"
+            activeClassName="border-b-2 border-purple-600"
+            to={`/blog/`}
+          >
+            Blog
           </Link>
-          <Link className="text-2xl text-purple-600" to={`/about`}>
-            about
+          <Link
+            className="text-xl md:text-2xl text-purple-600 pb-1"
+            activeClassName="border-b-2 border-purple-600"
+            to={`/about/`}
+          >
+            About
           </Link>
         </div>
       </nav>
