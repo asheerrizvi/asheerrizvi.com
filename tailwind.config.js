fe813b87bdaeef15ef1769683d4ff83161b3ba62
Asheer Rizvi <asheer.vil@gmail.com>
2020-09-15 23:00:39 +0530

    Changes to material-dark to avoid purging its CSS.

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,32 +56,11 @@ module.exports = {
             boxShadow:
               "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05) !important",
           },
-          "pre[class*=language-]": {
-            fontSize: "0.875rem",
-            borderRadius: "0",
-            margin: "1.25rem -1rem",
-            padding: "1.5rem",
-          },
-          "code[class*=language-text]": {
-            fontSize: "0.875rem",
-            fontWeight: "400",
-            padding: ".1em .3em",
-            borderRadius: ".3rem",
-            color: "#805AD5",
-            backgroundColor: "#f8f5ff",
-            textShadow: "none",
-          },
           "code::before": {
             content: "",
           },
           "code::after": {
             content: "",
-          },
-          "@media (min-width: 768px)": {
-            "pre[class*=language-]": {
-              borderRadius: "0.375rem",
-              margin: "1.25rem auto",
-            },
           },
         },
       },
