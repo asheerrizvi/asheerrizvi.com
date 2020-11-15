d36178508a203f4e5e8cfc34880ba27c2aa419ca
Asheer Rizvi <asheer.vil@gmail.com>
2020-11-15 12:46:03 +0530

    refact(styles): Cleaned up tailwind config, added default styles for mobile devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   purge: {
-    mode: "all",
     content: ["./src/**/*.js"],
   },
   theme: {
@@ -30,39 +29,6 @@ module.exports = {
         sm: "2rem",
         lg: "4rem",
         xl: "8rem",
-      },
-    },
-    typography: {
-      default: {
-        css: {
-          h1: {
-            fontWeight: "700",
-          },
-          h2: {
-            fontWeight: "600",
-          },
-          h3: {
-            fontWeight: "500",
-          },
-          h4: {
-            fontWeight: "400",
-          },
-          a: {
-            color: "#805AD5",
-            textDecoration: "none",
-          },
-          "img[class*=gatsby-resp-image-image]": {
-            borderRadius: "0.375rem",
-            boxShadow:
-              "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05) !important",
-          },
-          "code::before": {
-            content: "",
-          },
-          "code::after": {
-            content: "",
-          },
-        },
       },
     },
   },
